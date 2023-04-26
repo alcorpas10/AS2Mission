d63edbcd8507b4f251791cf9911e4a391e47273e
Alejandro Corpas Calvo <60468917+alcorpas10@users.noreply.github.com>
2023-04-26 13:34:52 +0200

    Record

--- simulation/rosbag.bash
+++ simulation/rosbag.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-ros2 bag record -o bag_prueba /tf /tf_static /clock /path0_left /path0_covered /path1_left /path1_covered /path2_left /path2_covered
+ros2 bag record -o $1 /tf /tf_static /clock /path0_left /path0_covered /path1_left /path1_covered /path2_left /path2_covered
