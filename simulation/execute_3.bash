8c78ed0c27af3e85838ca83693b01fcd8c9cfd21
Alejandro Corpas Calvo <60468917+alcorpas10@users.noreply.github.com>
2023-05-23 12:11:16 +0200

    New tests

--- simulation/execute_3.bash
+++ simulation/execute_3.bash
@@ -5,19 +5,19 @@ SESSION="Mision"
 tmux -2 new-session -d -s $SESSION
 
 tmux new-window -t $SESSION:1 -n 'Monitor 0'
-tmux send-keys "ros2 launch monitor execution_monitor.py drone_id:=0 config_file:=config/config_prueba_swarm2.yaml" C-m 
+tmux send-keys "ros2 launch monitor execution_monitor.py drone_id:=0 config_file:=config/config_prueba_swarm_3.yaml" C-m 
 
 tmux new-window -t $SESSION:2 -n 'Monitor 1'
-tmux send-keys "ros2 launch monitor execution_monitor.py drone_id:=1 config_file:=config/config_prueba_swarm2.yaml" C-m
+tmux send-keys "ros2 launch monitor execution_monitor.py drone_id:=1 config_file:=config/config_prueba_swarm_3.yaml" C-m
 
 tmux new-window -t $SESSION:3 -n 'Monitor 2'
-tmux send-keys "ros2 launch monitor execution_monitor.py drone_id:=2 config_file:=config/config_prueba_swarm2.yaml" C-m
+tmux send-keys "ros2 launch monitor execution_monitor.py drone_id:=2 config_file:=config/config_prueba_swarm_3.yaml" C-m
 
 tmux new-window -t $SESSION:4 -n 'Replanner'
 tmux send-keys "ros2 launch replanning_manager replanning_manager.py" C-m
 
 tmux new-window -t $SESSION:5 -n 'Viewer'
-tmux send-keys "ros2 launch viewer viewer.py config_file:=config/config_prueba_swarm2.yaml" C-m
+tmux send-keys "ros2 launch viewer viewer.py config_file:=config/config_prueba_swarm_3.yaml" C-m
 
 tmux new-window -t $SESSION:6 -n 'Mission Transmitter 0'
 tmux send-keys "python3 mission_transmitter.py 3 drone_sim_" C-m
